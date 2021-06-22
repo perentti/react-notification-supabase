6e59a8a1d1521e80dc69aa395117cf2e9bf8fa35
serge kouame <skouame@greenflex.com>
2021-06-22 11:29:05 +0200

    update menu-item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 
 function App() {
   return (
     <div>
-     <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
