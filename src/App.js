0941c23bb306b3b7c44fba79f0808ffab7a1b3cd
serge kouame <skouame@greenflex.com>
2021-06-23 17:44:33 +0200

    update app with shop page, collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = (props) => {
   console.log(props)
@@ -19,7 +20,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
